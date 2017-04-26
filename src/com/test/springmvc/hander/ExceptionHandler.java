1604c85ac7f3dac1126df7ff368a903e1c0f657e
yanjie1234 <137103000@qq.com>
2017-04-27 01:35:33 +0800

    test

---
+++ src/com/test/springmvc/hander/ExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.test.springmvc.hander;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+public class ExceptionHandler implements HandlerExceptionResolver{
+
+	@Override
+	public ModelAndView resolveException(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2,
+			Exception arg3) {
+		return new ModelAndView("404");
+	}
+
+}
